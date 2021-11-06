37a634775d85ee5b15886b7509210baf6f2f36c1
re-beat <raihanr090@gmail.com>
2021-11-06 10:27:14 +0800

    first commit

---
+++ Assets/Scripts/UserDataManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserDataManager : MonoBehaviour
+{
+    private const string PROGRESS_KEY = "Progress";
+
+    public static UserProgressData Progress;
+
+    public static void Load()
+    {
+        if (!PlayerPrefs.HasKey(PROGRESS_KEY))
+        {
+            Progress = new UserProgressData();
+            Save();
+        }
+        else
+        {
+            string json = PlayerPrefs.GetString(PROGRESS_KEY);
+            Progress = JsonUtility.FromJson<UserProgressData>(json);
+        }
+    }
+    public static void Save()
+    {
+        string json = JsonUtility.ToJson(Progress);
+        PlayerPrefs.SetString(PROGRESS_KEY, json);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
